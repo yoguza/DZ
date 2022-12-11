f42fb0f378b0286ee82c4d732e3e699f31ea5950
tkachevdg <tkachevdg@gmail.com>
2022-12-11 23:55:41 +0600

    сумма положительных элементов массива

---
+++ 6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int i;
+int sum = 0;
+for (int n = 0; n < 4; n++)
+{
+
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out i);
+    if (i > 0);
+    sum = sum + i;
+}
+Console.Write($" Сумма положительных элементов = {sum} "); 
+/*if (sum > 0);
+    { Console.Write($" Сумма положительных элементов = {sum} ");   }
+else
+    {  Console.Write($" Положительных элементов нет "); }*/
