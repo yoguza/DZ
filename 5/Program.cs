a2d75fc93fdb8c6114a4040bf47a75bc14ae38bf
tkachevdg <tkachevdg@gmail.com>
2022-12-11 22:47:08 +0600

    сумма нечетных элементов массива

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+    {
+    array[i]= new Random().Next(10);
+    Console.Write($"{array[i]} ");
+    }
+int S = array[1];
+for (int i = 3; i < array.Length; i = i + 2)
+    {
+        S = S + array[i];
+    }
+    Console.Write($"Сумма нечетных элементов = {S} ");
